0086711fd5b1d9066fc134d5817b9ddf7f538984
Carlos Santiago <carlosers@gmail.com>
2021-05-20 17:09:23 -0300

    create views, routes and templates

---
+++ website/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import hello_blog
+urlpatterns = [
+    path('', hello_blog),
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
 
-# Create your views here.
+#def hello_blog(request):
+ #   return HttpResponse("Blog")
+
+def hello_blog(request):
+    return render(request,'index.html')
+
